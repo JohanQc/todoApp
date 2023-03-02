61362d74b5ed36b3185abdbbd528fe909d04d8e7
JohanCode <deimonsu@gmail.com>
2023-03-02 00:06:38 -0500

    chore: add components.

---
+++ src/components/todoApp.js
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import Todo from "./todo";
+
+import "./todoApp.css"
+
+export default function TodoApp() {
+  const [title, setTitle] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleChange(event) {
+    const value = event.target.value;
+    setTitle(value);
+  }
+
+  function hadleSubmit(e) {
+    e.preventDefault();
+
+    const newTodo = {
+      id: crypto.randomUUID(),
+      title: title,
+      isCompleted: false,
+    };
+
+    // const temp = [...todos];
+    // temp.unshift(newTodo);
+
+    if (title === "") {
+      alert("please, enter task's name")
+      return
+    }
+    setTodos([ newTodo, ...todos]);
+
+    setTitle("")
+  }
+
+  function handleUpdate(id, value) {
+    const temp = [...todos];
+    const item = temp.find((item) => item.id === id);
+    item.title = value;
+    setTodos(temp);
+  }
+
+  function handleDelete(id) {
+    const temp = todos.filter((item) => item.id !== id);
+
+    setTodos (temp)
+  }
+
+  return (
+    <div className="todoContainer">
+      <form className="todoCreateForm" onSubmit={hadleSubmit}>
+        <input onChange={handleChange} className="todoInput" value={title} placeholder="Task's name" />
+        <input
+          onClick={hadleSubmit}
+          type="submit"
+          value="Create todo"
+          className="buttonCreate"
+        />
+      </form>
+
+      <div className="todosContianer">
+        {todos.map((item) => (
+          <Todo
+            key={item.id}
+            item={item}
+            onUpdate={handleUpdate}
+            onDelete={handleDelete}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
